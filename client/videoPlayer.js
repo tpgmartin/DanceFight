a244e5ba808b5c7574844b36a8a55c2b5cf64870
tpgmartin <tpgmartin@gmail.com>
2014-11-13 09:17:29 +0000

    Hardcode &autoplay=1

--- client/videoPlayer.js
+++ client/videoPlayer.js
@@ -1,10 +1,20 @@
 Template.videoPlayer.events({
-  "click": function () {
+  "click": function (ev) {
      $("#loading").css("display", "none");
+     
      if ( $("button").hasClass("play") ) { 
        $('button').addClass('pause').removeClass('play');
+       $('.video').each(function(){
+          this.src += "&autoplay=1";
+          ev.preventDefault();
+       });
      } else {
        $('button').addClass('play').removeClass('pause');
+       $('.video').each(function(){
+          // Replace this 
+          this.src.replace("&autoplay=1","");
+          ev.preventDefault();
+        });
      }
   }
 });
