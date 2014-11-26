ade2d153a2713412e3e863e60ba5a2a5d7265af2
tpgmartin <tpgmartin@gmail.com>
2014-11-26 04:42:56 +0000

    Add basic YouTube API interface to videoPlayer.html

--- client/videoPlayer.js
+++ client/videoPlayer.js
@@ -1,14 +1,14 @@
 // Want to autoplay videos on load and automatically pause as screen loads
 // Add in play/pause button to add usability 
 Groups = [ {
-    title: 'Video 1',
+    title: 'Video1',
     id: '1',
-    url: 'https://www.youtube.com/embed/M7lc1UVf-VE?enablejsapi=1&controls=0&showinfo=0&rel=0'
+    url: 'https://www.youtube.com/embed/M7lc1UVf-VE?autoplay=0&enablejsapi=1&html5=1&controls=0&showinfo=0&rel=0'
   },
   {
     title: 'Video2',
 	id: '2',
-    url: 'http://www.youtube.com/embed/W7qWa52k-nE?enablejsapi=1&controls=0&showinfo=0&rel=0'
+    url: 'http://www.youtube.com/embed/W7qWa52k-nE?autoplay=0&enablejsapi=1&html5=1&controls=0&showinfo=0&rel=0'
 }
 ];
 
@@ -19,25 +19,42 @@ Template.videoPlayer.helpers({
 });
 
 Template.videoPlayer.events({
-  "click button.play": function (ev) {
-     $("#loading").css("display", "none");
-     
-     if ( $("button").hasClass("play") ) { 
-       $('button.play').addClass('pause').removeClass('play');
-       $('button.pause').text('PAUSE')
-       $('.video').each(function(){
-          this.src += "&autoplay=1";
-          ev.preventDefault();
-       });
-     } else {
-       $('button').addClass('play').removeClass('pause');
-       $('.video').each(function(){
-          // Replace this 
-          this.src.replace("&autoplay=1","");
-          ev.preventDefault();
-        });
-     }
+  "click button#play": function () {
+    player1.playVideo();
+    player2.playVideo();
+    $('#play').css('display', 'none');
+    $('#pause').css('display', 'inherit');
   },
+
+  "click button#pause": function () {
+    player1.pauseVideo();
+    player2.pauseVideo();
+    $('#pause').css('display', 'none');
+    $('#play').css('display', 'inherit');
+  },
+
+  // "click button.play": function () {
+  //   alert('asdas');
+  // },
+  // "click button.play": function (ev) {
+  //    $("#loading").css("display", "none");
+     
+  //    if ( $("button").hasClass("play") ) { 
+  //      $('button.play').addClass('pause').removeClass('play');
+  //      $('button.pause').text('PAUSE')
+  //      $('.video').each(function(){
+  //         this.src += "&autoplay=1";
+  //         ev.preventDefault();
+  //      });
+  //    } else {
+  //      $('button').addClass('play').removeClass('pause');
+  //      $('.video').each(function(){
+  //         // Replace this 
+  //         this.src.replace("&autoplay=1","");
+  //         ev.preventDefault();
+  //       });
+  //    }
+  // },
  
  "click button.vote": function (ev) {
 	alert('Voted for : Video ' + this.id);
